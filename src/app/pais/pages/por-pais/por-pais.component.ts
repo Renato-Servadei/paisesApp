6108596a68166355b2d64de31d543fe3b282b161
Rober <boblawyer1@gmail.com>
2022-07-13 17:33:34 -0300

    creados el sidebar, las rutas y el template de la busqueda por pais

--- src/app/pais/pages/por-pais/por-pais.component.ts
+++ src/app/pais/pages/por-pais/por-pais.component.ts
@@ -6,11 +6,16 @@ import { Component, OnInit } from '@angular/core';
   styles: [
   ]
 })
-export class PorPaisComponent implements OnInit {
+export class PorPaisComponent{
+
+  termino: string = "Vamos River"
 
   constructor() { }
 
-  ngOnInit(): void {
+  buscar() {
+    console.log(this.termino)
   }
+
+
 
 }
