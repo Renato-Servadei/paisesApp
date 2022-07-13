23eebf6783f820477001fbb9e9252262fafb28a2
Rober <boblawyer1@gmail.com>
2022-07-13 19:44:52 -0300

    creado el servicio pais y la interfaz Country

--- src/app/pais/pages/por-pais/por-pais.component.ts
+++ src/app/pais/pages/por-pais/por-pais.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { PaisService } from '../../services/pais.service';
 
 @Component({
   selector: 'app-por-pais',
@@ -8,12 +9,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PorPaisComponent{
 
-  termino: string = "Vamos River"
+  termino: string = "";
+  hayError: boolean = false;
 
-  constructor() { }
+  constructor(private paisService : PaisService) { }
 
   buscar() {
-    console.log(this.termino)
+    this.hayError = false;
+    this.paisService.buscarPais(this.termino)
+    .subscribe(paises => {
+      console.log(paises);
+    }, (err) => {
+      this.hayError = true;
+    });
+    
   }
 
 
